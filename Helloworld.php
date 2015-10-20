bd9e72b9a554bed5f3a73b2d015b19c50ef2fc43
sholeh <msholeh980@gmail.com>
2015-10-20 15:26:47 +0700

    Create Helloworld.php

---
+++ Helloworld.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link http://muhamadsholeh.com
+ * @package yii2-helloworld
+ * @version 1.0.0
+ */
+ 
+namespace msholeh980\helloworld;
+class Helloworld extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello World!";
+    }
+}
